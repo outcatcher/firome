eeb1743c6a09e510b90cb6a62e7c1b0c544fa71d
Anton Kachurin <katchuring@gmail.com>
2024-05-27 00:26:53 +0300

    Save laps data (#2)
    Resolve #1
    
    Add logging

---
+++ firome/logging.py
@@ -0,0 +1,8 @@
+import logging
+from sys import stdout
+
+__hdl = logging.StreamHandler(stdout)
+__hdl.setFormatter(logging.Formatter())  # TODO: выбрать нормальный формат
+
+LOGGER = logging.Logger("merger", logging.INFO)
+LOGGER.addHandler(__hdl)
