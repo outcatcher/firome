51ae60c7bb13eeddc42ba9ae995b8a072ee55881
Anton Kachurin <katchuring@gmail.com>
2024-05-28 21:09:54 +0300

    Add route interpolation

--- firome/merge.py
+++ firome/merge.py
@@ -1,27 +1,33 @@
 from .parsers.fit import parse_fit
 from .parsers.gpx import parse_gpx
+from .parsers.gpx_interpolate import interpolate
 from .types.points import Point
 
 
 def merge(route_path: str, recording_path: str) -> list[Point]:
-    position_elements = parse_gpx(route_path)
+    position_elements = interpolate(parse_gpx(route_path), 5.0)
+
     data_elements = parse_fit(recording_path)
 
     p_i = d_i = 0
     # todo: возможно, придётся пре-аллоцировать память, если будут большие файлы
     result = []  # [None] * (len(position_elements)+len(data_elements))
 
-    last_timestamp = data_elements[0].timestamp
+    last_point = data_elements[0]
 
     while d_i < len(data_elements):
         if p_i < len(position_elements) and position_elements[p_i].distance < data_elements[d_i].distance:
-            # допущение, ожидаемое число точек на треке намного меньше точек из FIT файла
-            position_elements[p_i].timestamp = last_timestamp
+            # align position data with activity data
+            position_elements[p_i].timestamp = last_point.timestamp
+            position_elements[p_i].speed = last_point.speed
+            position_elements[p_i].power = last_point.power
+            position_elements[p_i].cadence = last_point.cadence
+            position_elements[p_i].heart_rate = last_point.heart_rate
             result.append(position_elements[p_i])
             p_i += 1
 
         result.append(data_elements[d_i])
-        last_timestamp = data_elements[d_i].timestamp
+        last_point = data_elements[d_i]
 
         d_i += 1
 
---
+++ firome/parsers/gpx_interpolate.py
@@ -0,0 +1,173 @@
+# MIT License
+#
+# Copyright (c) 2019 Remi Salmon, 2024 Anton Kachurin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Original implementation:
+# https://github.com/remisalmon/gpx-interpolate/blob/00af3c636d566d049f6a140c093af4e91d0482d5/gpx_interpolate.py
+# TODO: rewrite without list-dict-list conversions
+from datetime import tzinfo
+from typing import Dict, List, Union
+
+# TODO: get rid of numpy and scipy, they're bloating binary
+import numpy as np
+from scipy.interpolate import pchip_interpolate
+
+from firome.logging import LOGGER
+from firome.types.points import Point
+
+# types
+GPXData = Dict[str, Union[List[float], tzinfo, None]]
+
+# globals
+EARTH_RADIUS = 6371e3  # meters
+EPS = 1e-6  # seconds
+
+
+# functions
+def gpx_interpolate(gpx_data: GPXData, res: float = 5.0) -> GPXData:
+    """
+    Returns gpx_data interpolated with a spatial resolution res using piecewise cubic Hermite splines.
+
+    if num is passed, gpx_data is interpolated to num points and res is ignored.
+    """
+
+    if all(gpx_data[i] in (None, []) for i in ("lat", "lon", "ele", "dist")):
+        return gpx_data
+
+    _gpx_data = gpx_remove_duplicates(gpx_data)
+    _gpx_dist = gpx_calculate_distance(_gpx_data, use_ele=True)
+
+    xi = np.cumsum(_gpx_dist)
+    yi = np.array([_gpx_data[i] for i in ("lat", "lon", "ele", "dist") if _gpx_data[i]])
+
+    num = int(np.ceil(xi[-1] / res))
+
+    x = np.linspace(xi[0], xi[-1], num=num, endpoint=True)
+    y = pchip_interpolate(xi, yi, x, axis=1)
+
+    gpx_data_interp = {
+        "lat": list(y[0, :]),
+        "lon": list(y[1, :]),
+        "ele": list(y[2, :]) if gpx_data["ele"] else None,
+        "dist": list(y[3, :]),
+    }
+
+    return gpx_data_interp
+
+
+def gpx_calculate_distance(gpx_data: GPXData, use_ele: bool = True) -> List[float]:
+    """
+    Returns the distance between GPX trackpoints.
+
+    if use_ele is True and gpx_data['ele'] is not None, the elevation data is used to compute the distance.
+    """
+
+    gpx_dist = np.zeros(len(gpx_data["lat"]))
+
+    for i in range(len(gpx_dist) - 1):
+        lat1 = np.radians(gpx_data["lat"][i])
+        lon1 = np.radians(gpx_data["lon"][i])
+        lat2 = np.radians(gpx_data["lat"][i + 1])
+        lon2 = np.radians(gpx_data["lon"][i + 1])
+
+        delta_lat = lat2 - lat1
+        delta_lon = lon2 - lon1
+
+        c = 2.0 * np.arcsin(
+            np.sqrt(np.sin(delta_lat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(delta_lon / 2.0) ** 2)
+        )  # haversine formula
+
+        dist_latlon = EARTH_RADIUS * c  # great-circle distance
+
+        if gpx_data["ele"] and use_ele:
+            dist_ele = gpx_data["ele"][i + 1] - gpx_data["ele"][i]
+
+            gpx_dist[i + 1] = np.sqrt(dist_latlon**2 + dist_ele**2)
+        else:
+            gpx_dist[i + 1] = dist_latlon
+
+    return gpx_dist.tolist()
+
+
+def gpx_remove_duplicates(gpx_data: GPXData) -> GPXData:
+    """
+    Returns gpx_data where duplicate trackpoints are removed.
+    """
+
+    gpx_dist = gpx_calculate_distance(gpx_data, use_ele=False)
+
+    i_dist = np.concatenate(([0], np.nonzero(gpx_dist)[0]))  # keep gpx_dist[0] = 0.0
+
+    if len(i_dist) == len(gpx_dist):
+        return gpx_data
+
+    gpx_data_nodup = {"lat": [], "lon": [], "ele": [], "dist": []}
+
+    for k in ("lat", "lon", "ele", "dist"):
+        gpx_data_nodup[k] = [gpx_data[k][i] for i in i_dist] if gpx_data[k] else None
+
+    return gpx_data_nodup
+
+
+def from_track(track: list[Point]) -> GPXData:
+    """
+    Returns a GPXData structure from a GPX file.
+    """
+
+    gpx_data = {"lat": [], "lon": [], "ele": [], "dist": []}
+
+    for point in track:
+        gpx_data["lat"].append(point.position[0])
+        gpx_data["lon"].append(point.position[1])
+        gpx_data["ele"].append(point.elevation)
+        gpx_data["dist"].append(point.distance)
+
+    return gpx_data
+
+
+def to_track(gpx_data: GPXData) -> list[Point]:
+    gpx_track = []
+
+    for i in range(len(gpx_data["lat"])):
+        lat = gpx_data["lat"][i]
+        lon = gpx_data["lon"][i]
+        dist = gpx_data["dist"][i]
+        ele = gpx_data["ele"][i] if gpx_data["ele"] else None
+
+        gpx_point = Point(position=(lat, lon), elevation=ele, distance=dist)
+
+        gpx_track.append(gpx_point)
+
+    return gpx_track
+
+
+# main
+def interpolate(track: list[Point], res=10.0) -> list[Point]:
+    gpx_data = from_track(track)
+
+    gpx_data_nodup = gpx_remove_duplicates(gpx_data)
+
+    if not len(gpx_data_nodup["lat"]) == len(gpx_data["lat"]):
+        LOGGER.warn("Removed {} duplicate trackpoint(s)".format(len(gpx_data["lat"]) - len(gpx_data_nodup["lat"])))
+
+    gpx_data_interp = gpx_interpolate(gpx_data_nodup, res)
+
+    return to_track(gpx_data_interp)
