ea28c39125f058edc26d5fdb62c02f2cfed39d43
Anton Kachurin <katchuring@gmail.com>
2024-08-04 22:50:37 +0300

    Embed app version (#29)
    Add app version to window header
    
    Add app version to merged file (fix #23)
    
    Add `--version` cli flag

--- firome/__main__.py
+++ firome/__main__.py
@@ -1,22 +1,36 @@
 import argparse
 import logging
+import sys
 import time
 
+from firome import __version__
+from firome.codecs.tcx import export_as_tcx
 from firome.logger import LOGGER
 from firome.merge import merge
-from firome.codecs.tcx import export_as_tcx
+
+
+def __no_prio_args():
+    return "--version" not in sys.argv
+
 
 parser = argparse.ArgumentParser(
     description="Combines GPX file with training data with GPX file with position data based on the distance"
 )
-parser.add_argument("--route", type=str, required=True, help="Path to GPX file with route of the training")
-parser.add_argument("--recording", type=str, required=True, help="Path to FIT file with GPS-less data of the training")
+parser.add_argument("--route", type=str, required=__no_prio_args(),
+                    help="Path to GPX file with route of the training")
+parser.add_argument("--recording", type=str, required=__no_prio_args(),
+                    help="Path to FIT file with GPS-less data of the training")
 parser.add_argument("--precision", type=float, default=1.0, help="Precision of interpolation, meters")
 parser.add_argument("--output", choices=["tcx"], default="tcx", help="Output format")
 parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+parser.add_argument("--version", action="store_true", help="Print app version")
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+        sys.exit(0)
 
     LOGGER.info("route: %s", args.route)
     LOGGER.info("recording: %s", args.recording)
