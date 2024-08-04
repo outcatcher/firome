bd15f29aecbee701db772cbf9635709212bb64cb
Anton Kachurin <katchuring@gmail.com>
2024-08-04 10:57:14 +0300

    Fix logic

--- firome/merge.py
+++ firome/merge.py
@@ -14,7 +14,7 @@ def merge(position_elements: list[PositionPoint], data_elements: list[DataPoint]
     while d_i < d_len:
         for p_i in range(p_start, p_len):
             if data_elements[d_i].distance is None:
-                continue
+                break  # exit internal loop
 
             if math.isclose(position_elements[p_i].distance, data_elements[d_i].distance, abs_tol=precision / 2):
                 data_elements[d_i].position = position_elements[p_i].position
