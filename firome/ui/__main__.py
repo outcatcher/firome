1a828faaf31cef0b7348818c9cf0822e340dec01
Anton Kachurin <katchuring@gmail.com>
2024-10-06 20:55:53 +0300

    Fix fluctuating timestamps in loaded activity file
    Implement #24

--- firome/ui/__main__.py
+++ firome/ui/__main__.py
@@ -1,12 +1,31 @@
+import argparse
+import logging
 import sys
 
 from PySide6.QtWidgets import QApplication
 
+from firome import __version__
+from firome.logger import LOGGER
 from firome.ui.main import MainWindow
 
 app = QApplication(sys.argv)
 
+parser = argparse.ArgumentParser(
+    description="Combines GPX file with training data with GPX file with position data based on the distance"
+)
+parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+parser.add_argument("--version", action="store_true", help="Print app version")
+
 if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+        sys.exit(0)
+
+    if args.debug:
+        LOGGER.setLevel(logging.DEBUG)
+
     mw = MainWindow()
     mw.show()
 
